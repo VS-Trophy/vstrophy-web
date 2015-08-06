1d16f210f7882573fc5cd3cfb1d7d5ee81227acd
burninghammer <fabian.chanton@gmx.ch>
2015-08-06 20:38:38 +0200

    Fixed height of user table

--- WebPortal/src/main/java/ch/burninghammer/vstrophy/webportal/gui/useradministration/UserAdministrationViewImpl.java
+++ WebPortal/src/main/java/ch/burninghammer/vstrophy/webportal/gui/useradministration/UserAdministrationViewImpl.java
@@ -37,7 +37,7 @@ public class UserAdministrationViewImpl extends AbstractMVPView implements UserA
     private HorizontalLayout mainLayout;
 
     @Inject
-    @TableProperties(immediate = true, sizeUndefined = true)
+    @TableProperties(immediate = true, sizeUndefined = true, pageLength = 10)
     private Table userTable;
 
     @Inject
