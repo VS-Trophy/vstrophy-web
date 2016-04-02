f4447977fca9d683f51513a7fdad19bfa6056379
burninghammer <fabian.chanton@gmx.ch>
2016-04-03 00:33:08 +0200

    Add navbar skeleton

--- JS-WebPortal/app/app.component.ts
+++ JS-WebPortal/app/app.component.ts
@@ -1,9 +1,9 @@
 import {Component} from 'angular2/core';
-import {Alert} from 'ng2-bootstrap/ng2-bootstrap';
+import {NavigationComponent} from './navigation/navigation.component';
 
 @Component({
     selector: 'my-app',
-      directives: [Alert],
-    template: '<alert type="info">ng2-bootstrap hello world!</alert>'
+    directives: [NavigationComponent],
+    template:  '<vst-navigation></vst-navigation>'
 })
 export class AppComponent { }
---
+++ JS-WebPortal/app/navigation/navigation.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'vst-navigation',
+    templateUrl: 'app/navigation/navigation.html'
+})
+export class NavigationComponent { }
