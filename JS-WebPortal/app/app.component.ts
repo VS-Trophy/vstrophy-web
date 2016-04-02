296d286edd29ce5ab468b48a37c49e54d0802017
burninghammer <fabian.chanton@gmx.ch>
2016-04-02 23:59:58 +0200

    Add ng2-bootstrap

--- JS-WebPortal/app/app.component.ts
+++ JS-WebPortal/app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {Alert} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 3 App</h1>'
+      directives: [Alert],
+    template: '<alert type="info">ng2-bootstrap hello world!</alert>'
 })
 export class AppComponent { }
