c9498de339e941785668232149f1a0c9a0080333
burninghammer <fabian.chanton@gmx.ch>
2016-04-03 09:57:17 +0200

    Add news item class

---
+++ JS-WebPortal/app/news/news-item.js
@@ -0,0 +1,17 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var NewsItem;
+    return {
+        setters:[],
+        execute: function() {
+            NewsItem = (function () {
+                function NewsItem() {
+                }
+                return NewsItem;
+            }());
+            exports_1("NewsItem", NewsItem);
+        }
+    }
+});
+//# sourceMappingURL=news-item.js.map
---
+++ JS-WebPortal/app/news/news-item.ts
@@ -0,0 +1,7 @@
+export class NewsItem {
+  id: number;
+  title: string;
+  author: string;
+  timestamp: Date;
+  text: string;
+}
