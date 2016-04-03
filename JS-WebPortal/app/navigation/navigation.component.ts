a7d31dbd5a23060de5db6db5dcccb74d1c19873d
burninghammer <fabian.chanton@gmx.ch>
2016-04-03 15:55:44 +0200

    improve navbar

--- JS-WebPortal/app/navigation/navigation.component.ts
+++ JS-WebPortal/app/navigation/navigation.component.ts
@@ -2,12 +2,11 @@ import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES } from 'angular2/router';
 
 
+
 @Component({
     selector: 'vst-navigation',
     templateUrl: 'app/navigation/navigation.html',
     directives: [ROUTER_DIRECTIVES]
 })
 export class NavigationComponent {
-       
-    
- }
+}
