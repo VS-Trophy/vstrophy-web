5a75d78d1e952e42e5ff9280c8e25f5ed5476659
burninghammer <fabian.chanton@gmx.ch>
2016-04-24 21:13:49 +0200

    Basic Team Rest API

---
+++ REST-API/src/main/java/ch/vstrophy/rest/api/TeamApi.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.vstrophy.rest.api;
+
+import ch.vstrophy.entities.news.NewsItem;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+
+
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author kobashi@burninghammer.ch
+ */
+@Path("/team")
+public interface TeamApi {
+    
+    @GET
+    @Path("/")
+    @Produces("application/json")
+    public Response getTeams();
+
+}   
+    
