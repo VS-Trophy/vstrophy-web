26b5f5d10d6de5237b36e00520ca03445a0726b5
burninghammer <fabian.chanton@gmx.ch>
2016-05-04 22:09:24 +0200

    improve news item component (forgot a file)

--- JS-WebPortal/app/news/component/news-item/news-item.component.ts
+++ JS-WebPortal/app/news/component/news-item/news-item.component.ts
@@ -2,11 +2,13 @@ import {Component} from 'angular2/core';
 import {OnInit} from 'angular2/core';
 import {NewsService} from '../../service/news.service';
 import {NewsItem} from '../../model/news-item';
-import {RouteParams, } from 'angular2/router';
+import {RouteParams,ROUTER_DIRECTIVES } from 'angular2/router';
 
 @Component({
     selector: 'vst-news-item',
-    templateUrl: 'app/news/component/news-item/news-item.component.html'
+    templateUrl: 'app/news/component/news-item/news-item.component.html',
+    styleUrls: ['app/news/component/news-item/news-item.component.css'],
+    directives: [ROUTER_DIRECTIVES]
 })
 export class NewsItemComponent implements OnInit {
     private _newsItem: NewsItem;
