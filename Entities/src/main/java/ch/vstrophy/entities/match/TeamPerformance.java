32d5ffcb17426b2ac4777b6e9ae12a67f3812a49
Fabian <fabian.chanton@gmx.ch>
2018-02-27 21:17:16 +0100

    Team Performance entity

---
+++ Entities/src/main/java/ch/vstrophy/entities/match/TeamPerformance.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.vstrophy.entities.match;
+
+/**
+ *
+ * @author Fabian Chanton <fabian.chanton@gmx.ch>
+ */
+public class TeamPerformance {
+  private double points;
+
+  public double getPoints() {
+    return points;
+  }
+
+  public void setPoints(double points) {
+    this.points = points;
+  }
+  
+  
+}
