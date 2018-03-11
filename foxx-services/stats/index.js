eef30279f3f6f1dbbc4fa89752a172aa82b9904b
Fabian <fabian.chanton@gmx.ch>
2018-03-11 17:12:11 +0100

    Improved the stats foxx service

--- foxx-services/stats/index.js
+++ foxx-services/stats/index.js
@@ -8,41 +8,79 @@ const errors = require('@arangodb').errors;
 const teamCollection = db._collection('VSTrophyTeams');
 const DOC_NOT_FOUND = errors.ERROR_ARANGO_DOCUMENT_NOT_FOUND.code;
 
-
-
 module.context.use(router);
 
-// continued
-router.get('/performances/:topflop', function (req, res) {
-  try {
 
-    const ascDesc = mapTopFlop(req.pathParams.topflop)
+router.get('/performances/', function (req, res) {
+  try {
+    const ascDesc = req.queryParams.sortorder
+    const week = req.queryParams.week
+    const season = req.queryParams.season
     const limit = req.queryParams.limit
     const performances = 
-    db._query(queries.topPerformances(ascDesc,limit))
+    db._query(queries.topPerformances(ascDesc,limit,week,season))
     res.send(performances)
   } catch (e) {
     if (!e.isArangoError || e.errorNum !== DOC_NOT_FOUND) {
       throw e;
     }
-    res.throw(404, 'The entry does not exist', e);
+    res.throw(500, 'Could not get performances', e);
   }
 })
-.pathParam('topflop', joi.string().allow('top').allow('flop'), 'Either top or flop depending on, if the best or worst performances should be returned')
-.queryParam('limit',joi.number().integer().positive().default(100), 'How many performances should be returned')
+.queryParam('sortorder',joi.string().allow("desc").allow("asc").insensitive().default('desc'), 'Sort order. ASC or DESC')
+.queryParam('limit',joi.number().integer().positive().default(10), 'How many matches should be returned')
+.queryParam('season',joi.number().integer().positive().default(null), 'If set only results of this season are displayed')
+.queryParam('week',joi.number().integer().positive().default(null), 'If set only results of this week are displayed')
 .response(['application/json'], 'An Object containing all important informations regarding the performance')
 .summary('Best or worst team performances')
 .description('Returns the best or worst team performances');
 
-
-function mapTopFlop(topflop){
-  switch(topflop.toUpperCase()){
-    case 'TOP':
-      return 'DESC';
-    case 'FLOP':
-    return 'ASC';
-    default:
-    throw "Unexpected value. Expected either TOP or FLOP but got " + topFlop
+router.get('/matches/margin', function (req, res) {
+  try {
+    const ascDesc = req.queryParams.sortorder
+    const week = req.queryParams.week
+    const season = req.queryParams.season
+    const limit = req.queryParams.limit
+    const matches = 
+    db._query(queries.closestMatches(ascDesc,limit,week,season))
+    res.send(matches)
+  } catch (e) {
+    if (!e.isArangoError || e.errorNum !== DOC_NOT_FOUND) {
+      throw e;
+    }
+    res.throw(500, 'Could not get matches', e);
   }
-  
-}
+}).queryParam('sortorder',joi.string().allow("desc").allow("asc").insensitive().default('asc'), 'Sort order. ASC or DESC')
+.queryParam('limit',joi.number().integer().positive().default(10), 'How many matches should be returned')
+.queryParam('season',joi.number().integer().positive().default(null), 'If set only results of this season are displayed')
+.queryParam('week',joi.number().integer().positive().default(null), 'If set only results of this week are displayed')
+.response(['application/json'], 'An Object containing all important informations regarding the matches')
+.summary('Highest or lowest scoring matches')
+.description('Returns the closest or clearest matches');
+
+
+
+router.get('/matches/totalpoints', function (req, res) {
+  try {
+    const ascDesc = req.queryParams.sortorder
+    const week = req.queryParams.week
+    const season = req.queryParams.season
+    const limit = req.queryParams.limit
+    const matches = 
+    db._query(queries.highestScoringMatches(ascDesc,limit,week,season))
+    res.send(matches)
+  } catch (e) {
+    if (!e.isArangoError || e.errorNum !== DOC_NOT_FOUND) {
+      throw e;
+    }
+    res.throw(500, 'Could not get matches', e);
+  }
+})
+.queryParam('sortorder',joi.string().allow("desc").allow("asc").insensitive().default('desc'), 'Sort order. ASC or DESC')
+.queryParam('limit',joi.number().integer().positive().default(10), 'How many matches should be returned')
+.queryParam('season',joi.number().integer().positive().default(null), 'If set only results of this season are displayed')
+.queryParam('week',joi.number().integer().positive().default(null), 'If set only results of this week are displayed')
+.response(['application/json'], 'An Object containing all important informations regarding the matches')
+.summary('Highest or lowest scoring matches')
+.description('Returns the closest or clearest matches');
+
--- foxx-services/stats/queries/queries.js
+++ foxx-services/stats/queries/queries.js
@@ -1,23 +1,59 @@
 const aql = require('@arangodb').aql;
-module.exports.topPerformances = function(ascdesc, limit){
+module.exports.topPerformances = function(ascdesc,limit,week,season){
 return aql`
 FOR teamPerformance IN TeamPlayedIn
 SORT teamPerformance.points ${ascdesc}
 
-LIMIT ${limit}
+
 LET team = DOCUMENT(teamPerformance._from)
 LET match = DOCUMENT(teamPerformance._to)
 
-LET seasonweek = FIRST(
 FOR week IN 1..1 ANY match MatchesInWeek
+FILTER ${week}==null || week.number == ${week} 
 FOR season IN 1..1 ANY week WeeksInSeason
-RETURN {"season" : season.number, "week" : week.number}
-)
-
+FILTER ${season}==null || season.number == ${season} 
+LET seasonweek =  {"season" : season.number, "week" : week.number}
+LIMIT ${limit}
 LET opponentPerformance = FIRST(
 FOR opp,oppPerf IN 1..1 ANY match TeamPlayedIn
 FILTER opp!=team
 RETURN {"opponent" : opp.name, "points" : oppPerf.points}
 ) 
 RETURN {"points" : teamPerformance.points, "team" : team.name, "season" : seasonweek.season, "week": seasonweek.week, "opponent": opponentPerformance.opponent, "opponentPoints" : opponentPerformance.points }`
+}
+
+module.exports.closestMatches = function(ascdesc,limit,week,season){
+return aql`FOR match IN Matches
+LET performances = (FOR team,performance IN 1..1 ANY match TeamPlayedIn
+RETURN {"team" : team.name, "points" : performance.points})
+LET margin = ABS(performances[0].points - performances[1].points)
+SORT margin ${ascdesc}
+
+FOR week IN 1..1 ANY match MatchesInWeek
+FILTER ${week}==null || week.number == ${week} 
+FOR season IN 1..1 ANY week WeeksInSeason
+FILTER ${season}==null || season.number == ${season} 
+LET seasonweek =  {"season" : season.number, "week" : week.number}
+
+LIMIT ${limit}
+RETURN {"firstTeam": performances[0].team,"firstTeamPoints": performances[0].points,"secondTeam": performances[1].team,"secondTeamPoints": performances[1].points, "margin" : margin,"season" : seasonweek.season, "week": seasonweek.week}`
+}
+
+module.exports.highestScoringMatches = function(ascdesc,limit,week,season){
+    return aql`FOR match IN Matches
+    LET performances = (FOR team,performance IN 1..1 ANY match TeamPlayedIn
+    RETURN {"team" : team.name, "points" : performance.points})
+    LET score = performances[0].points + performances[1].points
+    SORT score ${ascdesc}
+    
+    FOR week IN 1..1 ANY match MatchesInWeek
+    FILTER ${week}==null || week.number == ${week} 
+    FOR season IN 1..1 ANY week WeeksInSeason
+    FILTER ${season}==null || season.number == ${season} 
+    LET seasonweek =  {"season" : season.number, "week" : week.number}
+    
+    LIMIT ${limit}
+    RETURN {"firstTeam": performances[0].team,"firstTeamPoints": performances[0].points,"secondTeam": performances[1].team,"secondTeamPoints": performances[1].points, "totalScore" : score,"season" : seasonweek.season, "week": seasonweek.week}`
+    
+
 }
