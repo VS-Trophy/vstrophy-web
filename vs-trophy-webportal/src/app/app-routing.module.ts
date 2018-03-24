fb10ac23dfe4ace2e83330a3fe48850071547b20
Fabian <fabian.chanton@gmx.ch>
2018-03-24 20:09:50 +0100

    Add router outlet and a first router link

--- vs-trophy-webportal/src/app/app-routing.module.ts
+++ vs-trophy-webportal/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { HistoryViewComponent } from './history/history-view/history-view.component';
+
+const routes: Routes = [
+  {path: 'history', component: HistoryViewComponent}
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
