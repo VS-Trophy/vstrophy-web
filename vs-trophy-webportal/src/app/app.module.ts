3148a4c08ecf5ce40c095f2e3dd2a9c7c41f5572
Fabian <fabian.chanton@gmx.ch>
2018-03-25 20:42:12 +0200

    Add ng-bootstrap module and bootstrap css

--- vs-trophy-webportal/src/app/app.module.ts
+++ vs-trophy-webportal/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
@@ -16,16 +17,19 @@ import { CoreModule } from './core/core.module';
     AppComponent
   ],
   imports: [
+    //From angular
     HttpClientModule,
     BrowserModule,
-
+    //Routing
     AppRoutingModule,
-
+    //Core
     CoreModule,
-
+    //Features
     TeamsModule,
     MatchesModule,
-    ResultsModule
+    ResultsModule,
+    //Third party
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
