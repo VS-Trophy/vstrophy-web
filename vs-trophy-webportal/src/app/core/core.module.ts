b8731c85f5e96b2bb7a21c9e2e374bc013099e5a
Fabian <fabian.chanton@gmx.ch>
2018-05-06 16:44:49 +0200

    Add basic material navbar

--- vs-trophy-webportal/src/app/core/core.module.ts
+++ vs-trophy-webportal/src/app/core/core.module.ts
@@ -2,14 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExceptionService } from './exception.service';
 import { AppRoutingModule } from '../app-routing.module';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [
     ExceptionService
-  ]
+  ],
+  declarations: [NavigationComponent],
+  exports: [NavigationComponent]
 })
 export class CoreModule { }
