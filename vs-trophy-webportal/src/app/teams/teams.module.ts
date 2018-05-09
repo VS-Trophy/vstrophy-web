5a77ab755a0a89ca760cf579089eb5ae3347f6b9
Fabian <fabian.chanton@gmx.ch>
2018-05-09 22:51:25 +0200

    Improve team overview

--- vs-trophy-webportal/src/app/teams/teams.module.ts
+++ vs-trophy-webportal/src/app/teams/teams.module.ts
@@ -5,12 +5,18 @@ import { TeamsOverviewComponent } from './teams-overview/teams-overview.componen
 import { TeamInfoComponent } from './team-info/team-info.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { StatsModule } from '../stats/stats.module';
+import {MatCardModule} from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   imports: [
     CommonModule,
     AppRoutingModule,
-    StatsModule
+    StatsModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatGridListModule
   ],
   providers: [
     TeamsService
