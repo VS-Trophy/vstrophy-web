b2daf9e7bc7e821253064e3b4307ec84f6251932
Fabian <fabian.chanton@gmx.ch>
2018-07-20 22:56:46 +0200

    Improve team overview

--- vs-trophy-webportal/src/app/teams/teams-overview/teams-overview.component.ts
+++ vs-trophy-webportal/src/app/teams/teams-overview/teams-overview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TeamsService } from '../teams.service';
 import { VSTrophyTeam } from '../vstrophyteam';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'vst-teams-overview',
@@ -9,7 +10,7 @@ import { VSTrophyTeam } from '../vstrophyteam';
 })
 export class TeamsOverviewComponent implements OnInit {
 
-  constructor(private teamsService:TeamsService) { }
+  constructor(private teamsService:TeamsService, private router: Router) { }
 
   teams: VSTrophyTeam[]
 
@@ -19,6 +20,10 @@ export class TeamsOverviewComponent implements OnInit {
 
   private loadAllTeams(){
     this.teamsService.getAllTeams().subscribe(teams => this.teams = teams)
+  }
+
+  cardClicked(team: VSTrophyTeam){
+    this.router.navigate(['teams/' + team.nflId])
   }
 
 }
