90b1ddd3fdf60d59f051edda3c08be7aa056467b
Fabian <fabian.chanton@gmx.ch>
2018-07-28 21:22:25 +0200

    Add date relevant dependencies

--- vs-trophy-webportal/src/app/app.module.ts
+++ vs-trophy-webportal/src/app/app.module.ts
@@ -12,6 +12,9 @@ import { TeamsModule } from './teams/teams.module';
 import { CoreModule } from './core/core.module';
 
 import {MatSelectModule} from '@angular/material/select';
+import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+
 import { NewsModule } from './news/news.module';
 
 
@@ -41,7 +44,12 @@ import { NewsModule } from './news/news.module';
     //Third party
 
   ],
-  providers: [],
+  providers: [
+    //Locale and date format for angular material components
+    {provide: MAT_DATE_LOCALE, useValue: 'de-CH'},
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
