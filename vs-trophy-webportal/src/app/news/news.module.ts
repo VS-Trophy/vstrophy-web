defd7659eca16eacdef218d49c1a969a2d3a75a9
Fabian <fabian.chanton@gmx.ch>
2018-08-01 23:10:33 +0200

    Improve editor slightly

--- vs-trophy-webportal/src/app/news/news.module.ts
+++ vs-trophy-webportal/src/app/news/news.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NewsItemEditorComponent } from './news-item-editor/news-item-editor.component';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import { MatButtonModule, MatListModule, MatDividerModule } from '@angular/material';
+import { MatButtonModule, MatListModule, MatDividerModule, MatCardModule } from '@angular/material';
 import { NewsItemEditorViewComponent } from './news-item-editor-view/news-item-editor-view.component';
 import { FormsModule } from '@angular/forms';
 import { NewsItemListComponent } from './news-item-list/news-item-list.component';
@@ -20,6 +20,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatButtonModule,
     MatListModule,
     MatDividerModule,
+    MatCardModule,
     //Flex Layout
     FlexLayoutModule
   ],
