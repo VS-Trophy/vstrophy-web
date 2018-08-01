9fed512f5929715e868fbfb7c8323b15f6d004d3
Fabian <fabian.chanton@gmx.ch>
2018-08-01 22:20:01 +0200

    Very basic layout for news editor view and list

--- vs-trophy-webportal/src/app/news/news.module.ts
+++ vs-trophy-webportal/src/app/news/news.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NewsItemEditorComponent } from './news-item-editor/news-item-editor.component';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import { MatButtonModule, MatListModule } from '@angular/material';
+import { MatButtonModule, MatListModule, MatDividerModule } from '@angular/material';
 import { NewsItemEditorViewComponent } from './news-item-editor-view/news-item-editor-view.component';
 import { FormsModule } from '@angular/forms';
 import { NewsItemListComponent } from './news-item-list/news-item-list.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
@@ -17,7 +18,10 @@ import { NewsItemListComponent } from './news-item-list/news-item-list.component
     MatInputModule,
     MatDatepickerModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    MatDividerModule,
+    //Flex Layout
+    FlexLayoutModule
   ],
   declarations: [NewsItemEditorComponent, NewsItemEditorViewComponent, NewsItemListComponent]
 })
