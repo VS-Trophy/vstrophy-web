470cf8bd715f0c2c6107dcc0515503ea86c305b3
Fabian <fabian.chanton@gmx.ch>
2018-08-05 18:07:19 +0200

    Get news items from backend

--- vs-trophy-webportal/src/app/news/news-item.service.ts
+++ vs-trophy-webportal/src/app/news/news-item.service.ts
@@ -1,30 +1,30 @@
 import { Injectable } from '@angular/core';
 import { NewsItem } from './news-item';
 import { Observable,from } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { catchError } from 'rxjs/operators';
+import { ExceptionService } from '../core/exception.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsItemService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private exceptionService: ExceptionService) { }
 
   getAllNewsItems(): Observable<NewsItem[]> {
-    const items: NewsItem[] =[
-      {
-        author: "ich",
-        content: "ihms content",
-        publicationDate: (new Date()).getDate(),
-        title: "Der Artikul"
-      }, {
-        author: "der andru",
-        content: "ihms content isch hie bizi anersch",
-        publicationDate: (new Date()).getDate(),
-        title: "Der aner Artikl"
-      }
-    ];
+    return this.http.get<NewsItem[]>(environment.apiRoot + `newsItems/`)
+      .pipe(
+        catchError(this.exceptionService.handleHttpError("getNewsItems", []))
+      );
+  }
 
-    return from(new Promise(resolve => resolve(items)));
+  getAllNewsItemsPaged(count: number, offset: number): Observable<NewsItem[]> {
+    return this.http.get<NewsItem[]>(environment.apiRoot + `newsItems/?count=${count}&offset=${offset}`)
+      .pipe(
+        catchError(this.exceptionService.handleHttpError("getNewsItems", []))
+      );
   }
 
   updateNewsItem(modifiedItem: NewsItem){
