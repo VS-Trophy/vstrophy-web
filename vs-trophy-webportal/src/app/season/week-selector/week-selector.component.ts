17e0482be559d518b4dd048dcb0646929f3806ad
Fabian <fabian.chanton@gmx.ch>
2018-09-01 23:00:59 +0200

    Improve week selector

--- vs-trophy-webportal/src/app/season/week-selector/week-selector.component.ts
+++ vs-trophy-webportal/src/app/season/week-selector/week-selector.component.ts
@@ -10,33 +10,49 @@ import { WeekPointer } from '../week-pointer';
 export class WeekSelectorComponent implements OnInit {
 
   constructor(private seasonsService: SeasonsService) { }
-
+  private defaultWeekNumbers: number[]
   weekNumbers: number[]
   selectedWeek: number
   selectedSeason: number
   @Output() selectedWeekChange = new EventEmitter<WeekPointer>();
 
   ngOnInit() {
+    this.defaultWeekNumbers = [-1];
+    this.defaultWeekNumbers.push(...Array.from({ length: 17 }, (v, k) => k + 1));
     this.weekNumbers = [-1];
   }
 
   onSeasonSelected(season: number) {
     this.selectedSeason = season
     if (season != -1) {
-      this.seasonsService.getWeekNumbers(season).subscribe(weeks => { this.weekNumbers.push(...weeks); this.setWeek(-1) })
+      this.weekNumbers = [-1];
+      this.seasonsService.getWeekNumbers(season).subscribe(weeks => {
+        this.weekNumbers.push(...weeks);
+        if (!this.weekNumbers.includes(this.selectedWeek)) {
+          this.setWeek(-1)
+        }
+        this.emitEvent();
+      })
+    } else {
+      this.weekNumbers = this.defaultWeekNumbers;
+      this.emitEvent();
     }
   }
-  
-  getValueName(season: number){
-    if(season == -1){
+
+  getValueName(season: number) {
+    if (season == -1) {
       return "Alle";
     }
     return season;
   }
 
+  emitEvent() {
+    this.selectedWeekChange.emit(new WeekPointer(this.selectedSeason, this.selectedWeek));
+  }
+
   setWeek(week: number) {
     this.selectedWeek = week
-    this.selectedWeekChange.emit(new WeekPointer(this.selectedSeason, this.selectedWeek))
+    this.emitEvent();
   }
 
 
