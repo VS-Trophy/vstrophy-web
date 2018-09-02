7626a6e1d084b79ba9793abaf84bb9011b07c0e7
Fabian <fabian.chanton@gmx.ch>
2018-09-02 20:24:33 +0200

    Make navigation responsive

--- vs-trophy-webportal/src/app/core/navigation/navigation.component.ts
+++ vs-trophy-webportal/src/app/core/navigation/navigation.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'vst-navigation',
@@ -8,6 +7,5 @@ import { Observable } from 'rxjs';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
-  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  
 }
