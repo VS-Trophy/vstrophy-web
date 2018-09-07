5c406a07c32dbbe2c708fbac2427296b543110dd
Fabian <fabian.chanton@gmx.ch>
2018-09-07 23:41:27 +0200

    Add division to the team query

--- foxx-services/teams/queries/queries.js
+++ foxx-services/teams/queries/queries.js
@@ -2,5 +2,7 @@ const aql = require('@arangodb').aql;
 
 module.exports.allTeams = function(){
     return aql`FOR team IN VSTrophyTeams
-    return team`
+        FOR division IN 1..1 INBOUND team TeamsInDivision
+        LET divisionAttribute = {"division" : division.name}
+    return merge(divisionAttribute, team)`
 }
