bc0af0ffaf1c40f04fff676f49af6c580b48c06f
Fabian <fabian.chanton@gmx.ch>
2018-10-20 09:52:10 +0200

    Add logo to table and make the whole thing more responsive

--- vs-trophy-webportal/src/app/stats/point-stats.ts
+++ vs-trophy-webportal/src/app/stats/point-stats.ts
@@ -1,12 +1,13 @@
 export class PointStats {
-    team: string
-    teamName: string
-    max: number
-    min: number
-    total: number
-    matches: number
-    average: number
-    ratio: number
-    wins: number
-    losses:number
+    team: string;
+    teamName: string;
+    teamLogoPath;
+    max: number;
+    min: number;
+    total: number;
+    matches: number;
+    average: number;
+    ratio: number;
+    wins: number;
+    losses: number;
 }
--- vs-trophy-webportal/src/app/stats/teams-table/teams-table.component.ts
+++ vs-trophy-webportal/src/app/stats/teams-table/teams-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { StatsService } from '../stats.service';
 import { PointStats } from '../point-stats';
 import { MatSort, MatTableDataSource } from '@angular/material';
@@ -13,17 +13,20 @@ import { tap } from 'rxjs/operators';
 export class TeamsTableComponent implements OnInit {
 
   constructor(private statsService: StatsService, private teamService: TeamsService) { }
-  displayedColumns: string[] = ['teamName','total','matches', 'average', 'max', 'min','ratio','wins','losses'];
+  displayedColumns: string[] = ['teamName', 'total', 'matches', 'average', 'max', 'min', 'ratio', 'wins', 'losses'];
   @ViewChild(MatSort) sort: MatSort;
   dataSource = new MatTableDataSource();
 
   ngOnInit() {
     this.dataSource.sort = this.sort;
     this.statsService.getAllTeamPointStats().pipe(
-      tap(stats => stats.forEach(stat => this.teamService.getTeamById(stat.team).subscribe(team => {console.info("got team "  + team + " for " + stat.team);stat.teamName = team.name})))
-    ).subscribe(
-      data => this.dataSource.data = data
-    )
+      tap(stats =>
+        stats
+          .forEach(
+            stat =>
+              this.teamService.getTeamById(stat.team)
+              .subscribe(team => { stat.teamName = team.name; stat.teamLogoPath = team.logoPath; })))
+    ).subscribe(data => this.dataSource.data = data);
 
   }
 
