2cd7e715228a29b56c38747f93edcaea21e41019
Fabian <fabian.chanton@gmx.ch>
2019-04-14 16:31:26 +0200

    Add stats for kickers and defensive players

--- golem/golem/gameparser.py
+++ golem/golem/gameparser.py
@@ -1,4 +1,4 @@
-from .items import MatchItemVST,  RosterItemVST, RosterSpotItemVST, OffensivePlayerPerformanceItemVST
+from .items import MatchItemVST,  RosterItemVST, RosterSpotItemVST, OffensivePlayerPerformanceItemVST, KickPlayerPerformanceItemVST, DefensivePlayerPerformanceItemVST
 
 
 def parse_game(response):
@@ -89,28 +89,78 @@ def create_base_player_roster_spot(player_row):
 def create_offense_roster_spot(player_row):
     roster_spot = create_base_player_roster_spot(player_row)
     performance = OffensivePlayerPerformanceItemVST(
-        points=float(player_row.css("span.playerTotal::text").get().replace("-","0")),
-        pass_yds=float(player_row.css("span.statId-5::text").get().replace("-","0")),
-        pass_tds=int(player_row.css("span.statId-6::text").get().replace("-","0")),
-        pass_ints=int(player_row.css("span.statId-7::text").get().replace("-","0")),
-        rush_yds=float(player_row.css("span.statId-14::text").get().replace("-","0")),
-        rush_tds=int(player_row.css("span.statId-15::text").get().replace("-","0")),
-        rec_yds=float(player_row.css("span.statId-21::text").get().replace("-","0")),
-        rec_tds=int(player_row.css("span.statId-22::text").get().replace("-","0")),
-        fum_tds=int(player_row.css("span.statId-29::text").get().replace("-","0")),
-        two_pts=int(player_row.css("span.statId-32::text").get().replace("-","0")),
-        fum_lost=int(player_row.css("span.statId-30::text").get().replace("-","0"))
+        points=float(player_row.css(
+            "span.playerTotal::text").get().replace("-", "0")),
+        pass_yds=float(player_row.css(
+            "span.statId-5::text").get().replace("-", "0")),
+        pass_tds=int(player_row.css(
+            "span.statId-6::text").get().replace("-", "0")),
+        pass_ints=int(player_row.css(
+            "span.statId-7::text").get().replace("-", "0")),
+        rush_yds=float(player_row.css(
+            "span.statId-14::text").get().replace("-", "0")),
+        rush_tds=int(player_row.css(
+            "span.statId-15::text").get().replace("-", "0")),
+        rec_yds=float(player_row.css(
+            "span.statId-21::text").get().replace("-", "0")),
+        rec_tds=int(player_row.css(
+            "span.statId-22::text").get().replace("-", "0")),
+        fum_tds=int(player_row.css(
+            "span.statId-29::text").get().replace("-", "0")),
+        two_pts=int(player_row.css(
+            "span.statId-32::text").get().replace("-", "0")),
+        fum_lost=int(player_row.css(
+            "span.statId-30::text").get().replace("-", "0"))
     )
     roster_spot["performance"] = performance
     return roster_spot
 
 
 def create_kicker_roster_spot(player_row):
-    return dict(create_base_player_roster_spot(player_row))
+    roster_spot = create_base_player_roster_spot(player_row)
+    performance = KickPlayerPerformanceItemVST(
+        points=float(player_row.css(
+            "span.playerTotal::text").get().replace("-", "0")),
+        pats=int(player_row.css(
+            "span.statId-33::text").get().replace("-", "0")),
+        twenty_minus_made=int(player_row.css(
+            "span.statId-35::text").get().replace("-", "0")),
+        twenty_thirty_made=int(player_row.css(
+            "span.statId-36::text").get().replace("-", "0")),
+        thirty_forthy_made=int(player_row.css(
+            "span.statId-37::text").get().replace("-", "0")),
+        forthy_fifty_made=int(player_row.css(
+            "span.statId-38::text").get().replace("-", "0")),
+        fifty_plus_made=int(player_row.css(
+            "span.statId-39::text").get().replace("-", "0"))
+    )
+    roster_spot["performance"] = performance
+    return roster_spot
 
 
 def create_defense_roster_spot(player_row):
-    return dict(create_base_player_roster_spot(player_row))
+    roster_spot = create_base_player_roster_spot(player_row)
+    performance = DefensivePlayerPerformanceItemVST(
+         points=float(player_row.css(
+            "span.playerTotal::text").get().replace("-", "0")),
+        sacks=int(player_row.css(
+              "span.statId-45::text").get().replace("-", "0")),
+        ints=int(player_row.css(
+              "span.statId-46::text").get().replace("-", "0")),
+        fum_rec=int(player_row.css(
+              "span.statId-47::text").get().replace("-", "0")),
+        safeties=int(player_row.css(
+              "span.statId-49::text").get().replace("-", "0")),
+        def_tds=int(player_row.css(
+              "span.statId-50::text").get().replace("-", "0")),
+        ret_tds=int(player_row.css(
+              "span.statId-53::text").get().replace("-", "0")),
+        allowed_pts=int(player_row.css(
+              "span.statId-54::text").get().replace("-", "0")),
+    )
+    roster_spot["performance"] = performance
+    return roster_spot
+
 
 '''
     Expects the teamTotal div of a teamWrap of the gamecenter
