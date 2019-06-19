0b23c6dfb59dba72c0e971b3532a6d2d99f08d9e
Fabian <fabian.chanton@gmx.ch>
2019-06-19 21:51:28 +0200

    Port teams service

--- foxx-services/teams/queries/queries.js
+++ foxx-services/teams/queries/queries.js
@@ -1,8 +1,8 @@
 const aql = require('@arangodb').aql;
 
 module.exports.allTeams = function(){
-    return aql`FOR team IN VSTrophyTeams
-        FOR division IN 1..1 INBOUND team TeamsInDivision
+    return aql`FOR team IN teamsVST
+        FOR division IN 1..1 INBOUND team teamsInDivision
         LET divisionAttribute = {"division" : division.name}
     return merge(divisionAttribute, team)`
 }
