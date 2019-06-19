57485420314595c0aac6300146f86272b11275b3
Fabian <fabian.chanton@gmx.ch>
2019-06-19 22:34:05 +0200

    Port seasons service

--- foxx-services/seasons/queries/queries.js
+++ foxx-services/seasons/queries/queries.js
@@ -1,26 +1,26 @@
 const aql = require('@arangodb').aql;
 
 module.exports.seasons = function(){
-    return aql`FOR season in Seasons 
-    LET weeks =( FOR week IN 1..1 ANY season WeeksInSeason
-    SORT week.number
+    return aql`FOR season in seasons 
+    LET weeks =( FOR week IN 1..1 OUTBOUND season weeksInSeason
+    SORT week.numner
     return week.number)
-    sort season.number
-RETURN {"season" : season.number, "weeks" : weeks}`
+    sort season._key
+RETURN {"season" : season._key, "weeks" : weeks}`
 }
 
 module.exports.seasonNumbers = function(){
-    return aql`FOR season in Seasons
-    SORT season.number DESC
-    return season.number`
+    return aql`FOR season in seasons
+    SORT season._key DESC
+    return season._key`
 }
 
 
 module.exports.season = function(season){
-    return aql`FOR season in Seasons 
-    FILTER season.number == ${season}
-        LET weeks =( FOR week IN 1..1 ANY season WeeksInSeason
+    return aql`FOR season in seasons 
+    FILTER season._key == TO_STRING(${season})
+        LET weeks =( FOR week IN 1..1 OUTBOUND season weeksInSeason
         SORT week.number
         return week.number)
-    RETURN {"season" : season.number, "weeks" : weeks}`
+    RETURN {"season" : season._key, "weeks" : weeks}`
 }
