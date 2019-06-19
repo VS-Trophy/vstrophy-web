a9c747aa1236ecda97bf6e4bffaba66fab76e388
Fabian <fabian.chanton@gmx.ch>
2019-06-19 23:50:03 +0200

    Port matches service (probably)

--- foxx-services/matches/queries/queries.js
+++ foxx-services/matches/queries/queries.js
@@ -1,63 +1,66 @@
 const aql = require('@arangodb').aql;
 const weekmod = require('./week.js')
 module.exports.matches = function(season, week, team1, team2){
-    return aql`FOR season IN Seasons
-    FILTER  ${season} == null || season.number == ${season}
-        FOR week IN 1..1 OUTBOUND season WeeksInSeason
+    return aql`FOR season IN seasons
+    FILTER  ${season} == null || season._key == TO_STRING(${season})
+        FOR week IN 1..1 OUTBOUND season weeksInSeason
         FILTER ${week} == null || week.number == ${week}
-            FOR match IN 1..1 OUTBOUND week MatchesInWeek
-                FOR team,performance IN 1..1 INBOUND match TeamPlayedIn
-                SORT team.nflId
-                COLLECT daMatch = match._key INTO teamPerfs
-                    LET featuredTeams = [teamPerfs[0].team.nflId,teamPerfs[1].team.nflId]
-                    FILTER ${team1} == null  || POSITION(featuredTeams, ${team1})
-                    FILTER ${team2} == null || POSITION(featuredTeams, ${team2})
-                    SORT teamPerfs[0].season.number DESC, teamPerfs[0].week.number DESC
-                   RETURN 
-                    {"firstTeamId" : teamPerfs[0].team.nflId, 
-                    "firstTeamPoints" : teamPerfs[0].performance.points, 
-                    "secondTeamId" : teamPerfs[1].team.nflId, 
-                    "secondTeamPoints" : teamPerfs[1].performance.points,
-                    "season" : teamPerfs[0].season.number,
-                    "week" :  teamPerfs[0].week.number}`
+            FOR match IN 1..1 OUTBOUND week matchesInWeekVST
+                FOR roster,performance IN 1..1 INBOUND match rosterPlayedInVST
+                    FOR team IN 1..1 INBOUND roster rosterOfVST
+                    SORT team._key
+                    COLLECT daMatch = match._key INTO teamPerfs
+                        LET featuredTeams = [teamPerfs[0].team._key,teamPerfs[1].team._key]
+                        FILTER ${team1} == null  || POSITION(featuredTeams, ${team1})
+                        FILTER ${team2} == null || POSITION(featuredTeams, ${team2})
+                        SORT teamPerfs[0].season._key DESC, teamPerfs[0].week.number DESC
+                    RETURN 
+                        {"firstTeamId" : teamPerfs[0].team._key, 
+                        "firstTeamPoints" : teamPerfs[0].performance.points, 
+                        "secondTeamId" : teamPerfs[1].team._key, 
+                        "secondTeamPoints" : teamPerfs[1].performance.points,
+                        "season" : teamPerfs[0].season._key,
+                        "week" :  teamPerfs[0].week.number}`
 }
 
 module.exports.closestMatches = function(ascdesc,limit,season,week){
     var currentWeek =  weekmod.currentWeek()
     var currentSeason = weekmod.currentSeason();
-return aql`FOR match IN Matches
-LET performances = (FOR team,performance IN 1..1 ANY match TeamPlayedIn
+return aql`FOR match IN matchesVST
+LET performances = (FOR roster,performance IN 1..1 INBOUND match rosterPlayedInVST
+    FOR team IN 1..1 INBOUND roster rosterOfVST
 RETURN {"team" : team, "points" : performance.points})
 LET margin = ABS(performances[0].points - performances[1].points)
 SORT margin ${ascdesc}
 
-FOR week IN 1..1 ANY match MatchesInWeek
+FOR week IN 1..1 INBOUND match matchesInWeekVST
 FILTER ${week}==null || week.number == ${week} 
-FOR season IN 1..1 ANY week WeeksInSeason
-FILTER ${season}==null || season.number == ${season} 
-FILTER week.number != ${currentWeek} || season.number != ${currentSeason}
-LET seasonweek =  {"season" : season.number, "week" : week.number}
+FOR season IN 1..1 INBOUND week weeksInSeason
+FILTER ${season}==null || season._key == TO_STRING(${season})
+FILTER week.number != ${currentWeek} || season._key != TO_STRING(${currentSeason})
+LET seasonweek =  {"season" : season._key, "week" : week.number}
 
 LIMIT ${limit}
-RETURN {"firstTeamId": performances[0].team.nflId,"firstTeamPoints": performances[0].points,"secondTeamId": performances[1].team.nflId,"secondTeamPoints": performances[1].points, "margin" : margin,"season" : seasonweek.season, "week": seasonweek.week}`
+RETURN {"firstTeamId": performances[0].team._key,"firstTeamPoints": performances[0].points,"secondTeamId": performances[1].team._key,"secondTeamPoints": performances[1].points, "margin" : margin,"season" : seasonweek.season, "week": seasonweek.week}`
 }
 
 module.exports.highestScoringMatches = function(ascdesc,limit,season,week){
     var currentWeek =  weekmod.currentWeek()
     var currentSeason = weekmod.currentSeason();
-    return aql`FOR match IN Matches
-    LET performances = (FOR team,performance IN 1..1 ANY match TeamPlayedIn
+    return aql`FOR match IN matchesVST
+    LET performances = (FOR roster,performance IN 1..1 INBOUND match rosterPlayedInVST
+    FOR team IN 1..1 INBOUND roster rosterOfVST
     RETURN {"team" : team, "points" : performance.points})
     LET score = performances[0].points + performances[1].points
     SORT score ${ascdesc}
     
-    FOR week IN 1..1 ANY match MatchesInWeek
+    FOR week IN 1..1 INBOUND match matchesInWeekVST
     FILTER ${week}==null || week.number == ${week} 
-    FOR season IN 1..1 ANY week WeeksInSeason
-    FILTER ${season}==null || season.number == ${season} 
-    FILTER week.number != ${currentWeek} || season.number != ${currentSeason}
-    LET seasonweek =  {"season" : season.number, "week" : week.number}
+    FOR season IN 1..1 INBOUND week weeksInSeason
+    FILTER ${season}==null || season._key == TO_STRING(${season})
+    FILTER week.number != ${currentWeek} || season._key != TO_STRING(${currentSeason})
+    LET seasonweek =  {"season" : season._key, "week" : week.number}
     
     LIMIT ${limit}
-    RETURN {"firstTeamId": performances[0].team.nflId,"firstTeamPoints": performances[0].points,"secondTeamId": performances[1].team.nflId,"secondTeamPoints": performances[1].points, "totalScore" : score,"season" : seasonweek.season, "week": seasonweek.week}`
+    RETURN {"firstTeamId": performances[0].team._key,"firstTeamPoints": performances[0].points,"secondTeamId": performances[1].team._key,"secondTeamPoints": performances[1].points, "totalScore" : score,"season" : seasonweek.season, "week": seasonweek.week}`
 }
