613e5aa2fb999e7b571b8644bb03878ab417a86c
Fabian <fabian.chanton@gmx.ch>
2019-07-13 22:32:00 +0200

    Remove uncalled query (only after having upgraded it)

--- foxx-services/stats/queries/queries.js
+++ foxx-services/stats/queries/queries.js
@@ -2,29 +2,6 @@ const aql = require('@arangodb').aql;
 const week = require('./week.js')
 
 
-module.exports.topPerformances = function (ascdesc, limit, week, season) {
-    return aql`
-FOR teamPerformance IN TeamPlayedIn
-SORT teamPerformance.points ${ascdesc}
-
-
-LET team = DOCUMENT(teamPerformance._from)
-LET match = DOCUMENT(teamPerformance._to)
-
-FOR week IN 1..1 ANY match MatchesInWeek
-FILTER ${week}==null || week.number == ${week} 
-FOR season IN 1..1 ANY week WeeksInSeason
-FILTER ${season}==null || season.number == ${season} 
-LET seasonweek =  {"season" : season.number, "week" : week.number}
-LIMIT ${limit}
-LET opponentPerformance = FIRST(
-FOR opp,oppPerf IN 1..1 ANY match TeamPlayedIn
-FILTER opp!=team
-RETURN {"opponent" : opp.name, "points" : oppPerf.points}
-) 
-RETURN {"points" : teamPerformance.points, "team" : team.name, "season" : seasonweek.season, "week": seasonweek.week, "opponent": opponentPerformance.opponent, "opponentPoints" : opponentPerformance.points }`
-}
-
 module.exports.winlossrecord = function (team, opponent, season) {
     var currentWeek = week.currentWeek()
     var currentSeason = week.currentSeason();
