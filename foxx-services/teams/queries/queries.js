c1f269e309835b1fc1f38aaa81aec3eeeb407917
Fabian <fabian.chanton@gmx.ch>
2019-09-09 21:54:12 +0200

    delete foxx service as it is moved to its own repository

--- foxx-services/teams/queries/queries.js
+++
@@ -1,8 +0,0 @@
-const aql = require('@arangodb').aql;
-
-module.exports.allTeams = function(){
-    return aql`FOR team IN teamsVST
-        FOR division IN 1..1 INBOUND team teamsInDivision
-        LET divisionAttribute = {"division" : division.name}
-    return merge(divisionAttribute, team)`
-}
