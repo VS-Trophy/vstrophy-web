a957edae953b9a9f457cf450bf03375184b1e5ed
Fabian <fabian.chanton@gmx.ch>
2022-08-14 15:28:58 +0200

    add azure environment

---
+++ vs-trophy-webportal/src/environments/environment.azure.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiRoot: "https://vstrophy-arangodb-app.wittyforest-3ab356b3.germanywestcentral.azurecontainerapps.io/_db/vs-trophy/"
+  };
+  
